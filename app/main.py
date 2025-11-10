e4481ba193587280c999b814203f25da8c65f3fb
Nikita Grinyov <nikita3grynov@gmail.com>
2025-11-10 18:27:59 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 def copy_file(command: str) -> None:
     command_parts = command.split()
-    if (command_parts[0] != "cp" or len(command_parts) < 3
-            or command_parts[1] == command_parts[2]):
+    if len(command_parts) < 3:
+        return
+    if (command_parts[0] != "cp" or command_parts[1] == command_parts[2]):
         return
     with (open(command_parts[1], "r") as source_file,
           open(command_parts[2], "w") as target_file):
