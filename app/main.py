6579d6adf6654a96c9018be90929fd5721f864d0
Nikita Grinyov <nikita3grynov@gmail.com>
2025-11-12 09:58:24 +0200

    fix

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ import os
 
 def copy_file(command: str) -> None:
     command_parts = command.split()
-    if len(command_parts) < 3:
+    if len(command_parts) != 3:
         return
     if (command_parts[0] != "cp" or command_parts[1] == command_parts[2]):
         return
